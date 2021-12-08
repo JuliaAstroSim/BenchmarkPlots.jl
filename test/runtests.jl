8dc828a44c472869394452325f93395c8dc42d13
islent <leoislent@gmail.com>
2021-12-08 09:56:20 +0800

    better function names

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ using Statistics
 @testset "Basic" begin
     scene, layout, df = benchmarkplot(
         [median, std],
+        string.([median, std]),
         rand,
         [10^i for i in 1:2],
     )
